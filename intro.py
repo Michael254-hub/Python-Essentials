6456f307b27ca3fe9bb75b49d9a980dd4be8f81f
Michael Simiyu <michaelsimiyu54@gmail.com>
2024-10-30 10:25:16 +0300

    first commit

---
+++ intro.py
@@ -0,0 +1,17 @@
+#Impoting Python Version
+
+import sys
+print(sys.version)
+
+#Python Version using the terminal
+"""
+Type Python (or py) on the command line being used to call the Python interpreter.
+Then type Python --version on the command line to print the current version of the Python interpreter you're using.
+"""
+
+#Python files
+"""
+Python files are recognized by the (.py) extension 
+"""
+
+# For commenting in Python or """Comment"""
