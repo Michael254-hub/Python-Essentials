b694e46a2e135f458a98df678c18a590cc1c7c50
Michael254-hub <michaelsimiyu@gmail.com>
2024-11-06 18:25:45 +0300

    updates

---
+++ input.py
@@ -0,0 +1,15 @@
+from collections.abc import async_generator
+
+name = input("Enter your firstname and lastname: ")
+print(name + " is logged in")
+
+gender = input("Enter you preferrable gender: ")
+print(gender)
+
+age = input("Enter your age: ")
+print(age)
+
+country = input("Enter your country: ")
+print(country)
+
+print("My name is " + name + ". A " + gender + " at the age of " + age + "years. I reside in " + country)
